fdf3f45dd1ef6321aadfd549a7ebfddf60cc2510
Ali Arif <91418777+aliarif1@users.noreply.github.com>
2022-08-27 02:58:44 +0500

    update
     seperate nav                     
      fade in on scroll                 
      fade in on scroll - services page
      seperate services page   
      skill hover title animation
      faq section
      populate footer
      email functionality
      scroll down animation

--- js/app.js
+++ js/app.js
@@ -36,3 +36,72 @@ function startCount(e) {
     }
   }, 20)
 }
+
+
+
+// Fade-in on page load 
+
+window.addEventListener("load" , () => {
+  // var heading = document.querySelector('.big-heading')
+  // heading.classList.add('active')
+  var reveals = document.querySelectorAll('.reveal')
+  reveals.forEach((revealItem) => {
+    if (revealItem.getBoundingClientRect().top < window.innerHeight) {
+      revealItem.classList.add("active")
+    }
+  })
+})
+
+
+// Fade animation on Scroll
+
+window.addEventListener("scroll", () => {
+
+  var reveals = document.querySelectorAll(".reveal")
+
+  reveals.forEach((revealItem) => {
+
+    var windowHeight = window.innerHeight
+    var revealItemTop = revealItem.getBoundingClientRect().top
+    var revealPoint = 80
+
+    if ((revealItemTop) < (windowHeight - revealPoint)) {
+      revealItem.classList.add("active")
+    } 
+    // else {
+    //   revealItem.classList.remove("active")
+    // }
+
+  })
+})
+
+
+// Email functionality
+
+const form = document.querySelector('form')
+const btn = document.querySelector('.btn-contact')
+
+btn.addEventListener('click', () => {
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "aliarif12333@gmail.com",
+    Password : "E5F7E41ADC41410ABB7D2ABE59A2BE95927C",
+    To : 'contact@techoniche.com',
+    From : form.elements["email"].value,
+    Subject : form.elements["subject"].value,
+    Body : form.elements["message"].value + "<br>" + form.elements["name"].value + "<br>" + form.elements["phone"].value
+  }).then(
+    message => alert("Email successufully sent!")
+  );
+})
+
+
+
+
+
+
+
+
+
+
+
